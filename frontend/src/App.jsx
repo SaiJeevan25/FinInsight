95d5fd36e23ea4dd92fa9eefbeb25013f60903ff
Ashishpathak78 <ap10ashu@gmail.com>
2025-02-27 15:57:10 +0530

    Adjusted App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,12 @@
-// import React from 'react'
-// // import WelcomePage from './Components/WelcomePage'
-// import Login from "./Components/login.jsx";
-// export default function App() {
-//   return (
-//     <div>
-//       {/* <WelcomePage /> */}
-//       <Login />
-//     </div>
-//   )
-// }
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Components/login";
-import Signup from "./Components/signup";
 import Welcomepage from "./Components/WelcomePage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/Welcome" element={<Welcomepage />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Welcomepage />} />
       </Routes>
     </Router>
   );
