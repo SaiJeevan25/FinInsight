2901c5db800b1e012a41511ce715db995c70c7f4
Sai Jeevan Battiprolu <battiprolusaijeevan@gmail.com>
2025-04-27 15:48:41 +0530

    added Quick Add option

--- backend/routes/otp_routes.py
+++ backend/routes/otp_routes.py
@@ -25,17 +25,77 @@ def send_otp():
     otp = str(random.randint(100000, 999999))
     otp_store[email] = {'otp': otp, 'expires': time.time() + 300}
     html_content = f"""
-<html>
-  <body style="font-family: Arial, sans-serif; line-height: 1.6;">
-    <p>Dear user,</p>
-    <p>Your <strong>One-Time Password (OTP)</strong> for the password reset is:</p>
-    <h2 style="color: #6366F1; background-color: #f1f1f1; display: inline-block; padding: 10px 15px; border-radius: 5px;">{otp}</h2>
-    <p>This code is valid for <strong>5 minutes</strong>.</p>
-    <p>If you did not request this, please ignore this email.</p>
-    <br>
-    <p>Best regards,<br><strong>FinInsight Team</strong></p>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset OTP</title>
+    <style>
+      body {{
+        font-family: 'Poppins', sans-serif;
+        line-height: 1.6;
+        background-color: #000000;
+        color: #333;
+        margin: 0;
+        padding: 20px;
+      }}
+      .container {{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 70vh;
+        padding: 20px;
+      }}
+      .email-content {{
+        background-color: #fff;
+        border-radius: 10px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        max-width: 600px;
+        width: 100%;
+        text-align: center;
+      }}
+      span{{
+        color: #4a4a4a;
+      }}
+      .header {{
+        font-size: 24px;
+        font-weight: bold;
+        color: #6366F1;
+      }}
+      .otp-container {{
+        color: #6366F1;
+        padding: 2px;
+        border-radius: 5px;
+        display: inline-block;
+        font-size: 22px;
+        font-weight: bold;
+      }}
+      .footer {{
+        font-size: 16px;
+        color: #555;
+      }}
+
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="email-content">
+        <p class="header">Fin<span>Insight</span></p>
+        <p>Dear User,</p>
+        <p>Your <strong>One-Time Password (OTP)</strong> for the password reset is:</p>
+        <div class="otp-container">
+          <p>{otp}</p>
+        </div>
+        <p>This code is valid for <strong>5 minutes</strong>.</p>
+        <p>If you did not request this, please ignore this email.</p>
+        <br>
+        <p class="footer">Best regards,<br><strong>FinInsight Team</strong></p>
+      </div>
+    </div>
   </body>
 </html>
+
 """
     msg = MIMEMultipart("alternative")
     msg["Subject"] = "Password Reset OTP"
